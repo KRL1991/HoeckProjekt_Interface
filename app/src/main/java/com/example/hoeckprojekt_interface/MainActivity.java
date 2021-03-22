bfbdd515afe065576209029228cb298dbd99de4e
KRL1991 <kimx4706@edu.easj.dk>
2021-03-22 14:56:20 +0100

    Added calculation of Bruttofortjeneste

--- app/src/main/java/com/example/hoeckprojekt_interface/MainActivity.java
+++ app/src/main/java/com/example/hoeckprojekt_interface/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.hoeckprojekt_interface;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void calculate (android.view.View view) {
+
+        EditText omsaetning = (EditText)findViewById(R.id.Omsaetning);
+        EditText vareforbrug = (EditText)findViewById(R.id.Vareforbrug);
+
+        EditText bruttoFortjeneste = (EditText)findViewById(R.id.Bruttofortjeneste);
+
+        Log.d("BruttoFortjeneste","Bruttofortjeneste er: " + bruttoFortjeneste);
+
+        int udregnetBruttoFortjeneste;
+        udregnetBruttoFortjeneste = Integer.valueOf(omsaetning.getText().toString()) + Integer.valueOf(vareforbrug.getText().toString());
+
+        bruttoFortjeneste.setText(String.valueOf(udregnetBruttoFortjeneste));
+
+
+
+
+
+
     }
 }
